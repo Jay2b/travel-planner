5eeda9ae6a5601d7dd119c57f29265bf64c903ad
Kuba <jakub.fieden@gmail.com>
2026-02-13 19:35:51 +0100

    Add trip edit and update feature

--- travel-planner_Frontend/src/App.jsx
+++ travel-planner_Frontend/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [trips, setTrips] = useState([]);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [editingId, setEditingId] = useState(null);
 
   function fetchTrips() {
     fetch(`http://localhost:3000/trips`)
@@ -17,10 +18,46 @@ function App() {
     fetchTrips();
   }, []);
 
+  //Add the trip
   function handleSubmit(event) {
     event.preventDefault();
-    fetch(`http://localhost:3000/trips`, {
-      method: "POST",
+
+    if (editingId) {
+      updateTrip(editingId);
+    } else {
+      fetch(`http://localhost:3000/trips`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name,
+          description,
+        }),
+      })
+        .then((res) => res.json())
+        .then(() => {
+          setName("");
+          setDescription("");
+          fetchTrips();
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
+  //Delete The trip
+  function deleteTrip(id) {
+    fetch(`http://localhost:3000/trips/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      setTrips((values) => {
+        return values.filter((trip) => trip.id !== id);
+      });
+    });
+  }
+
+  //Edit Trip
+  function updateTrip(id) {
+    fetch(`http://localhost:3000/trips/${id}`, {
+      method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         name,
@@ -28,12 +65,18 @@ function App() {
       }),
     })
       .then((res) => res.json())
-      .then((newTrip) => {
+      .then(() => {
+        setEditingId(null);
         setName("");
         setDescription("");
         fetchTrips();
       })
       .catch((err) => console.log(err));
+  }
+  function startEdit(trip) {
+    setEditingId(trip.id);
+    setName(trip.name);
+    setDescription(trip.description);
   }
 
   return (
@@ -52,12 +95,14 @@ function App() {
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
-        <button type='submit'>Send</button>
+        <button type='submit'>{editingId ? "Update Trip" : "Add Trip"}</button>
       </form>
       {trips.map((trip) => (
         <div key={trip.id}>
           <h3>{trip.name}</h3>
           <p>{trip.description}</p>
+          <button onClick={() => deleteTrip(trip.id)}>Delete</button>
+          <button onClick={() => startEdit(trip)}>Edit</button>
         </div>
       ))}
     </>
